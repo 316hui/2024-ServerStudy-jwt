33cc186f6689e6fcde9d7794de7ae7edb824abfe
316hui <dev316316316@gmail.com>
2024-08-06 10:58:45 +0900

    Initial commit

---
+++ jtwPrj/src/main/java/com/example/demo/JtwPrjApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtwPrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtwPrjApplication.class, args);
+	}
+
+}
